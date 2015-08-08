77801f269090e0b9726b5bf99a8658e3102bed16
Erlend Graff <erlend.graff@gmail.com>
2015-08-08 15:38:34 +0200

    Init

---
+++ release.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# Note: this script is based on the installer script from msysGit
+# (/share/msysGit/net/release.sh)
+
+# Recreate uit-thesis-install.exe
+
+TARGET="$HOME"/uit-thesis-install.exe
+TMPDIR=/tmp/ultinstaller-tmp
+OPTS7="-m0=lzma -mx=9 -md=64M"
+TMPPACK=/tmp.7z
+
+# Get script dir
+cd "$(dirname "${0}")"
+SHARE="$(pwd -L)"
+cd - > /dev/null
+
+test ! -d "$TMPDIR" || rm -rf "$TMPDIR" || exit
+mkdir "$TMPDIR" &&
+cd "$TMPDIR" &&
+(cd .. && test ! -f "$TMPPACK" || rm "$TMPPACK") &&
+echo "Copying files" &&
+sed 's/\r//g' "$SHARE"/fileList.txt |
+	(cd / && tar -c --file=- --files-from=-; echo $? > /tmp/exitstatus) |
+	tar xvf - &&
+test 0 = "$(cat /tmp/exitstatus)" &&
+sed 's/\r//g' "$SHARE"/fileList-mingw.txt |
+	(cd /mingw && tar -c --file=- --files-from=-;
+	 echo $? > /tmp/exitstatus) |
+	tar xvf - &&
+test 0 = "$(cat /tmp/exitstatus)" &&
+strip bin/*.exe libexec/git-core/*.exe &&
+mkdir etc &&
+cp "$SHARE"/gitconfig etc/ &&
+if test -d /etc/profile.d
+then
+	cp -R /etc/profile.d ./
+fi &&
+cp "$SHARE"/setup-ult.sh setup-ult.sh &&
+echo "Creating archive" &&
+cd .. &&
+/share/7-Zip/7za.exe a $OPTS7 "$TMPPACK" ultinstaller-tmp &&
+(cat /share/7-Zip/7zSD.sfx &&
+ echo ';!@Install@!UTF-8!' &&
+ echo 'Title="UiT thesis LaTeX template installation"' &&
+ echo 'GUIFlags="8+32+64+256+4096"' &&
+ echo 'GUIMode="1"' &&
+ echo 'OverwriteMode="2"' &&
+ echo 'RunProgram="\"%%T\ultinstaller-tmp\bin\sh.exe\" /setup-ult.sh"' &&
+ echo ';!@InstallEnd@!' &&
+ cat "$TMPPACK") > "$TARGET" &&
+echo "Success! You'll find the new installer at \"$TARGET\"." &&
+rm $TMPPACK
---
+++ setup-ult.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+# Note: this script is based on the setup script from msysGit
+# (/share/msysGit/net/setup-msysgit.sh)
+
+# We're already in the install directory
+INSTALL_PATH="$(pwd)"
+export PATH="$INSTALL_PATH/ultinstaller-tmp/bin:$PATH"
+
+error () {
+    echo "* error: $*"
+    echo INSTALLATION ABORTED
+    read -e IGNORED_INPUT
+    trap - exit
+    exit 1
+}
+
+echo
+echo -------------------------------------------------------
+echo Fetching the latest UiT thesis LaTeX template revision
+echo -------------------------------------------------------
+ULT_REPO_HTTP=https://github.com/egraff/uit-thesis.git
+
+# Multiply git.exe
+
+for builtin in init unpack-objects update-ref fetch ls-remote clone checkout
+do
+	ln "$INSTALL_PATH/ultinstaller-tmp/bin/git.exe" \
+		"$INSTALL_PATH/ultinstaller-tmp/bin/git-$builtin.exe"
+done
+
+git config --system http.sslCAinfo /bin/curl-ca-bundle.crt
+
+git init &&
+git config core.autocrlf true &&
+git config remote.origin.url $ULT_REPO_HTTP &&
+git config remote.origin.fetch \
+	+refs/heads/master:refs/remotes/origin/master &&
+git config branch.master.remote origin &&
+git config branch.master.merge refs/heads/master &&
+
+git fetch || {
+		echo -n "Please enter a HTTP proxy: " &&
+		read proxy &&
+		test ! -z "$proxy" &&
+		export http_proxy="$proxy" &&
+		export https_proxy="$proxy" &&
+		git fetch
+} ||
+error "Could not get uit-thesis.git"
+
+git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
+
+echo
+echo -------------------------------------------------------
+echo Checking out the uit-thesis master branch
+echo -------------------------------------------------------
+git checkout -l -f -q -b master origin/master ||
+	error Couldn\'t checkout the master branch!
+
+echo
+echo
+echo -------------------------------------------------------
+echo Installing UiT thesis LaTeX template
+echo -------------------------------------------------------
+make install CONTINUE=y
+
+echo
+echo
+echo -------------------------------------------------------
+echo Updating LaTeX database
+echo -------------------------------------------------------
+cmd.exe /c texhash
+
